cb8bfb22c58d5a4fed232d9037a938303d29a583
SaumyaKinnariKartikDalal <98890702+SaumyaKinnariKartikDalal@users.noreply.github.com>
2023-05-04 11:34:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftwristx = 0;
+rightwristx = 0;
+leftwristy = 0;
+rightwristy = 0;
 
 function preload() {
     song1 = loadSound("music.mp3");
@@ -14,9 +18,33 @@ function setup() {
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    //poseNet.on("pose", gotPoses);
+    poseNet.on("pose", gotPoses);
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill("red");
+    stroke("red");
+    circle(leftwristx, leftwristy, 10);
+    fill("blue");
+    stroke("blue");
+    circle(rightwristx, rightwristy, 10);
+}
+
+function modelLoaded() {
+    console.log("poseNet is Initialized");
+}
+
+function gotPoses(results) {
+    if (results.length>0) {
+        console.log(results);
+        leftwristx = results[0].pose.leftWrist.x;
+        console.log("leftwristx"+leftwristx);
+        rightwristx = results[0].pose.rightWrist.x;
+        console.log("rightwristx"+rightwristx);
+        leftwristy = results[0].pose.leftWrist.y;
+        console.log("leftwristy"+leftwristy);
+        rightwristy = results[0].pose.rightWrist.y;
+        console.log("rightwristy"+rightwristy);
+    }
 }
